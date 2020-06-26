967f7a9e15b615360ca5ca73eb254575575d524f
Tania <tania@pop-os.localdomain>
2020-06-27 01:59:41 +0600

    template mastering

---
+++ raggery_backend/src/components/layouts/Togglemenu.vue
@@ -0,0 +1,92 @@
+<template>
+    <div id = "togglemenu" >
+    <div class="modal fade apps-modal" id="appsModal" tabindex="-1" role="dialog" aria-labelledby="appsModalLabel" aria-hidden="true" data-backdrop="false">
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><i class="ik ik-x-circle"></i></button>
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="quick-search">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 ml-auto mr-auto">
+                            <div class="input-wrap">
+                                <input type="text" id="quick-search" class="form-control" placeholder="Search..." />
+                                <i class="ik ik-search"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-body d-flex align-items-center">
+                <div class="container">
+                    <div class="apps-wrap">
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-bar-chart-2"></i><span>Dashboard</span></a>
+                        </div>
+                        <div class="app-item dropdown">
+                            <a href="#" class="dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="ik ik-command"></i><span>Ui</span></a>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </div>
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-mail"></i><span>Message</span></a>
+                        </div>
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-users"></i><span>Accounts</span></a>
+                        </div>
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-shopping-cart"></i><span>Sales</span></a>
+                        </div>
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-briefcase"></i><span>Purchase</span></a>
+                        </div>
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-server"></i><span>Menus</span></a>
+                        </div>
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-clipboard"></i><span>Pages</span></a>
+                        </div>
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-message-square"></i><span>Chats</span></a>
+                        </div>
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-map-pin"></i><span>Contacts</span></a>
+                        </div>
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-box"></i><span>Blocks</span></a>
+                        </div>
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-calendar"></i><span>Events</span></a>
+                        </div>
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-bell"></i><span>Notifications</span></a>
+                        </div>
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-pie-chart"></i><span>Reports</span></a>
+                        </div>
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-layers"></i><span>Tasks</span></a>
+                        </div>
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-edit"></i><span>Blogs</span></a>
+                        </div>
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-settings"></i><span>Settings</span></a>
+                        </div>
+                        <div class="app-item">
+                            <a href="#"><i class="ik ik-more-horizontal"></i><span>More</span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+</template>
+
+export default {
+    name : "Togglemenu"
+}
