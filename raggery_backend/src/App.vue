037ad4072742b9d87020cf5ef38bd58c928e8827
ShakilAhmmed <shakilfci461@gmail.com>
2020-06-27 02:20:21 +0600

    Color Change

--- raggery_backend/src/App.vue
+++ raggery_backend/src/App.vue
@@ -1,60 +1,58 @@
 <template>
   <div id="app">
-   <div class="wrapper">
+    <div class="wrapper">
       <Topheader></Topheader>
       <div class="page-wrap">
         <Sidebar></Sidebar>
-          <div class="main-content">
-            <div class="container-fluid">
-                <div id ="main_app">
-                </div>
+        <div class="main-content">
+          <div class="container-fluid">
+            <div id="main_app">
             </div>
           </div>
-          <Rightsidebar></Rightsidebar>
-          <Chartpanel></Chartpanel>
-          <Footer></Footer>
-            
+        </div>
+        <Rightsidebar></Rightsidebar>
+        <Chartpanel></Chartpanel>
+        <Footer></Footer>
+
       </div>
-     
+
       <router-view/>
     </div>
     <Togglemenu></Togglemenu>
-    
+
   </div>
 
 </template>
 
 <script>
-import Sidebar from "./components/layouts/Sidebar";
-import Topheader from "./components/layouts/Topheader";
-import Rightsidebar from "./components/layouts/Rightsidebar";
-import Chartpanel from "./components/layouts/Chartpanel";
-import Footer from "./components/layouts/Footer";
-import Togglemenu from "./components/layouts/Togglemenu";
+  import Sidebar from "./components/layouts/Sidebar";
+  import Topheader from "./components/layouts/Topheader";
+  import Rightsidebar from "./components/layouts/Rightsidebar";
+  import Chartpanel from "./components/layouts/Chartpanel";
+  import Footer from "./components/layouts/Footer";
+  import Togglemenu from "./components/layouts/Togglemenu";
 
 
-
-
-export default {
-  name: 'App',
-  components:{
-   Sidebar,
-   Topheader,
-   Rightsidebar,
-   Chartpanel,
-   Footer,
-   Togglemenu,
-  },
-}
+  export default {
+    name: 'App',
+    components: {
+      Sidebar,
+      Topheader,
+      Rightsidebar,
+      Chartpanel,
+      Footer,
+      Togglemenu,
+    },
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
