0ac1cd7202086249a205730001c1e7138a59e070
ShakilAhmmed <shakilfci461@gmail.com>
2020-07-25 01:20:19 +0600

    Update

--- raggery_backend/src/components/layouts/Footer.vue
+++ raggery_backend/src/components/layouts/Footer.vue
@@ -1,15 +1,20 @@
 <template>
-   <div id = "footer">
+  <div id="footer">
     <footer class="footer">
-    <div class="w-100 clearfix">
+      <div class="w-100 clearfix">
         <span class="text-center text-sm-left d-md-inline-block">Copyright © Code Breakers.</span>
-        <span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Crafted with <i class="fa fa-heart text-danger"></i> by <a href="https://github.com/ShakilAhmmed/raggery-e-commerce" class="text-dark" target="_blank">Raggery</a></span>
-    </div>
-</footer>
-   </div>
+        <span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Crafted with <i
+          class="fa fa-heart text-danger"></i> by <a href="https://github.com/ShakilAhmmed/raggery-e-commerce"
+                                                     class="text-dark" target="_blank">Raggery</a></span>
+      </div>
+    </footer>
+  </div>
 
 </template>
+<script>
 
-export default {
-    name : "Footer"
-}
+  export default {
+    name: "Footer"
+  }
+
+</script>
